5a911d63f66ae2c5e2b96b054c37f7989b9da3bd
tsutsui.masayoshi0528@gmail.com <tsutsui.masayoshi0528@gmail.com>
2024-11-17 19:48:11 +0900

    client added

---
+++ client/gpuserver/retrieve.py
@@ -0,0 +1,17 @@
+# クライアント側のコード例
+import requests
+import base64
+
+response = requests.get('http://localhost:80/retrieve')
+data = response.json()
+
+if data['status'] == 'success':
+    # 画像の保存
+    img_content = base64.b64decode(data['data']['image']['content'])
+    with open(data['data']['image']['filename'], 'wb') as f:
+        f.write(img_content)
+    
+    # 音声の保存
+    sound_content = base64.b64decode(data['data']['sound']['content'])
+    with open(data['data']['sound']['filename'], 'wb') as f:
+        f.write(sound_content)
